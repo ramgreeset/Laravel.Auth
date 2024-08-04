7c2f2b62953e0ae0c2c2ea152c3a651b90d70782
Roman Artemyev <artemyev.ram@gmail.com>
2024-08-04 16:46:59 +0300

    Обновление проекта

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,20 +2,43 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
     //
-    public function create(){
+    public function create()
+    {
         return view('user.create');
     }
 
-    public function store(Request $request){
-        dd($request->all());
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'name' => ['required', 'max:255'],
+            'email' => ['required', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed'],
+        ]);
+
+
+        User::create($request->all());
+        return redirect()->route('login');
     }
 
-    public function login(){
+
+
+    public function login()
+    {
         return view('user.login');
+    }
+
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed'
+        ];
     }
 }
